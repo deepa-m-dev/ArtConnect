e7267613344051e0baa264042cdf0df15ca7f1ca
deepa-m-dev <sangeedeeps29@gmail.com>
2025-10-25 22:04:28 +0530

    updated project with new changes

--- src/main/java/com/example/ArtConnect/repository/ArtworkRepository.java
+++ src/main/java/com/example/ArtConnect/repository/ArtworkRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface ArtworkRepository extends MongoRepository<Artwork, String> {
-    List<Artwork> findByTagsIn(List<String> tags);
     List<Artwork> findByArtistId(String artistId);
 }
