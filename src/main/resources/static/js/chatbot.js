040d16bdb717f220fbdbaaf25f0bbfdbb921b4c8
Deepa M <deepamurugan2112@gmail.com>
2026-04-27 22:19:23 +0530

    Update chatbot.js

--- src/main/resources/static/js/chatbot.js
+++ src/main/resources/static/js/chatbot.js
@@ -8,7 +8,7 @@ function sendMessage() {
     let input = document.getElementById("userInput");
     let msg = input.value;
 
-    fetch("/api/chat", {
+    fetch("https://artconnect-chatbot.onrender.com/chat", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({message: msg})
